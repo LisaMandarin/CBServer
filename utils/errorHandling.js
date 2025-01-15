b2c861383b5ea415e8a5e452fb3287dd29e6eb30
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-01-15 18:24:16 +0800

    fix: fetch users

--- utils/errorHandling.js
+++ utils/errorHandling.js
@@ -1,7 +1,7 @@
 const errorHandling = (err, req, res, next) => {
     console.error(err.message)
 
-    res.status(err.status || 500).json({
+    res.status(err.status || err.code || 500).json({
         success: false,
         message: err.message || 'Internal server Error'
     })
